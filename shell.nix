f9558f3ba4b2ba7f5e80d507bf3ea186e8a11336
Damien Radtke <git@damienradtke.com>
2023-06-23 15:35:26 -0500

    Update Nix shell to define RUST_SRC_PATH

--- shell.nix
+++ shell.nix
@@ -3,8 +3,10 @@ pkgs.mkShell {
 	buildInputs = [
 		pkgs.gtk4
 		pkgs.pkg-config
-		pkgs.rustc
 		pkgs.cargo
 		pkgs.lua5_4
+		pkgs.rustc
+		pkgs.cargo
 	];
+	RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
