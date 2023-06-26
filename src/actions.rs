bb56c733d4228342be94f764a214cb6346bf5fec
Damien Radtke <git@damienradtke.com>
2023-06-26 15:09:56 -0500

    Render the About dialog

--- src/actions.rs
+++ src/actions.rs
@@ -1,9 +1,13 @@
 use gtk::gio;
 use gtk::glib;
+use gtk::prelude::*;
 
 pub fn about(_action: &gio::SimpleAction, _param: Option<&glib::Variant>) {
-    eprintln!("Showing the About dialog!");
-    let _ = gtk::AboutDialog::builder()
-        .authors(vec![String::from("Damien Radtke")])
-        .build();
+    gtk::AboutDialog::builder()
+        .program_name("Webby")
+        .authors(vec![String::from("Damien Radtke <me@damienradtke.com>")])
+        .comments("Program to render GTK applications using a web-based deployment model.")
+        .website("https://damienradtke.com/post/building-gtk-applications-like-websites/")
+        .build()
+        .set_visible(true);
 }
