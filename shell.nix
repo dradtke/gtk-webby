ea2b421a35f24165219f2d3a0b504c8630257129
Damien Radtke <git@damienradtke.com>
2023-07-07 10:59:49 -0500

    Make Nix shell reproducible

--- shell.nix
+++ shell.nix
@@ -1,23 +1,29 @@
-{ pkgs ? import <nixpkgs> {} }:
+# For an explanation of how this file works, see: https://nix.dev/tutorials/first-steps/nix-language#shell-environment
+#
+# Nixpkgs can be updated with: nix-shell -p niv --run "niv update"
+#
+
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs {} }:
 
 pkgs.mkShell {
-	buildInputs = [
-		pkgs.gtk4
-		pkgs.pkg-config
-		pkgs.lua5_4
-		pkgs.rustc
-		pkgs.cargo
+	buildInputs = with pkgs; [
+		gtk4
+		pkg-config
+		lua5_4
+		rustc
+		cargo
 	] ++ (
 		# SSL support is platform-specific
-		if pkgs.lib.strings.hasSuffix "-darwin" builtins.currentSystem then [
-			pkgs.darwin.apple_sdk.frameworks.Security
-			pkgs.darwin.apple_sdk.frameworks.CoreServices
-			pkgs.openssl
+		if pkgs.lib.strings.hasSuffix "-darwin" builtins.currentSystem then with pkgs; [
+			darwin.apple_sdk.frameworks.Security
+			darwin.apple_sdk.frameworks.CoreServices
+			openssl
 		]
-		else if pkgs.lib.strings.hasSuffix "-linux" builtins.currentSystem then [
-			pkgs.openssl
+		else if pkgs.lib.strings.hasSuffix "-linux" builtins.currentSystem then with pkgs; [
+			openssl
 		]
 		else []
 	);
+	# Needed for rust-analyzer to work with proc macros
 	RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
